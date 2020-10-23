37bf9c4fd0c31c7ff38f4ceb199487dc4507d54e
isak <isakhammer@gmail.com>
2020-10-23 15:30:18 +0200

    Log plot of cost function

--- nn_2.py
+++ nn_2.py
@@ -119,7 +119,10 @@ def train(c, d, d_0, K, h, Y, th, tau=0.0005, max_it=60):
     
     
     itr = 0
-    while (err > tol) and (itr < max_it ):
+    
+    JJ = np.zeros(max_it)
+    
+    while (itr < max_it ):
         
         Z, Upsilon = F_tilde(Y, th, d_0, d, K, h)
         I = Upsilon.shape[0]
@@ -150,10 +153,14 @@ def train(c, d, d_0, K, h, Y, th, tau=0.0005, max_it=60):
         th = gradientDesent(K, th, dJ_w, dJ_mu, dJ_W, dJ_b, tau)
         
         err = J_func(Upsilon,c)  
-        itr += 1
-        #print(itr,err)  
         
-    return err
+        JJ[itr] = err
+        
+        itr += 1
+        
+        
+        
+    return JJ
         
         
     
@@ -165,21 +172,25 @@ def main():
     d_0 = 2
     d = 4
     I = 20
+    max_it = 10000
+    
                 
     b = generate_synthetic_batches(I)
     c = b["c"]
     Y = b["Y"]
     d_0 = Y.shape[0]
-    th = initialize_weights(d_0, d, K)
     
     
-    KK = np.arange(10)
-    J = np.zeros(KK.shape)
+    KK = np.arange(1,4, 1)
+    it = np.arange(max_it)
     
     for i in range(len(KK)):    
-        J[i] = train(c, d, d_0, KK[i], h, Y, th, tau=0.0005, max_it=60)
-   
-    plt.plot(KK, J)
+        th = initialize_weights(d_0, d, KK[i])
+        JJ = train(c, d, d_0, KK[i], h, Y, th, tau=0.0005, max_it=max_it)
+        plt.plot(it, JJ, label="layer: "+ str(KK[i]))
+    
+    plt.yscale("log")
+    plt.legend()
     plt.show()
     
     
