d904f5faf421a9b4d5a6b82d05b86c7608ca103b
isak <isakhammer@gmail.com>
2020-10-23 19:57:33 +0200

    Removed generate data from nn_2

--- nn_2.py
+++ nn_2.py
@@ -7,29 +7,8 @@ Created on Thu Oct 22 17:29:57 2020
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
 
-
-def generate_synthetic_batches(I):
-    
-    batch = {} 
-    
-    d_0 = 2
-    
-    Y1 = np.linspace(-1,1,I)
-    Y2 = np.linspace(1,-1,I)
-    
-    batch["Y"] = np.array([Y1,Y2])
-    #batch["Y"] = np.array([[1,1],
-    #                      [1,2]])
-    
-    #batch["Y"] = np.random.uniform(high=2, low=-2, size=(d_0,I) )    
-    
-    
-    batch["c"] = 0.5*batch["Y"][0,:]**2 + 0.5*batch["Y"][1,:]**2
-    batch["c"] = batch["c"][:, np.newaxis]
-        
-    return batch
+from data import generate_synthetic_batches
 
 
 def F_tilde(Y, th, d_0, d, K, h):
@@ -162,7 +141,7 @@ def train(c, d, d_0, K, h, Y, th, tau=0.0005, max_it=60, print_it=False):
         JJ[itr+1] = err
         
         itr += 1
-        if(itr%10 == 0) and (print_it == True):
+        if(itr%50 == 0) and (print_it == True):
             print(itr,err)
         
     return JJ
